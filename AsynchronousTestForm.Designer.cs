1d370ae40122e0602d725fb9a4ef77bc488dcaf5
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2022-11-04 11:21:24 -0600

    Add project files.

---
+++ AsynchronousTestForm.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace FibonacciAsynchronous
+{
+    partial class AsynchronousTestForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.outputTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 25);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(433, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calls to Fibonacci(46) and Fibonacci(45) in separate threads";
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(24, 61);
+            this.startButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(432, 35);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = "Start Asynchronous Fibonacci Calls";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Location = new System.Drawing.Point(24, 112);
+            this.outputTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.outputTextBox.Multiline = true;
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.Size = new System.Drawing.Size(432, 285);
+            this.outputTextBox.TabIndex = 2;
+            // 
+            // AsynchronousTestForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(480, 410);
+            this.Controls.Add(this.outputTextBox);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "AsynchronousTestForm";
+            this.Text = "Asynchronous Test";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.TextBox outputTextBox;
+    }
+}
+
---
+++ AsynchronousTestForm.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FibonacciAsynchronous
+{
+    public partial class AsynchronousTestForm : Form
+    {
+        List<Task> tasks = new List<Task>();
+
+        public AsynchronousTestForm()
+        {
+            InitializeComponent();
+        }
+
+        // Start asynchronous calls to Fibonacci
+        private async void startButton_Click(object sender, EventArgs e)
+        {
+            outputTextBox.Text = "Starting Task to calculate Fibonacci(46)\r\n";
+
+            // Create Task to perform Fibonacci(46) calculation in thread
+            Task<TimeData> task1 = Task.Run(() => StartFibonacci(46));
+            tasks.Add(task1);
+
+            outputTextBox.AppendText("Starting Task to calculate Fibonacci(45)\r\n");
+
+            // Create Task to perform Fibonacci(45) calculation in thread
+            Task<TimeData> task2 = Task.Run(() => StartFibonacci(45));
+            tasks.Add(task2);
+
+            //await Task.WhenAll(task1, task2);  // Wait for both to complete
+            await Task.WhenAll(tasks);  // Wait for both to complete
+
+            // Determine time that first thread started
+            DateTime startTime = (task1.Result.StartTime < task2.Result.StartTime) ? task1.Result.StartTime : task2.Result.StartTime;
+
+            // Determine time that last thread ended
+            DateTime endTime = (task1.Result.EndTime < task2.Result.EndTime) ? task1.Result.EndTime : task2.Result.EndTime;
+
+            // Display total time for calculations 
+            double totalMinutes = (endTime - startTime).TotalMinutes;
+            outputTextBox.AppendText($"Total calculation time = {totalMinutes:F6} minutes\r\n");
+        }
+
+        // Start a call to Fibonacci and capture start/end times
+        TimeData StartFibonacci(int n)
+        {
+            // Create a TimeData object to store start/end times
+            var result = new TimeData();
+
+            AppendText($"Calculating Fibonacci({n})");
+            result.StartTime = DateTime.Now;
+            long fibonacciValue = Fibonacci(n);
+            result.EndTime = DateTime.Now;
+
+            AppendText($"Fibonacci({n}) = {fibonacciValue}");
+            double minutes = (result.EndTime - result.StartTime).TotalMinutes;
+            AppendText($"Calculation of time = {minutes:F6} minutes\r\n");
+
+            AppendText($"Total tasks in List<Task> = {tasks.Count()}\r\n");
+            return result;
+        }
+
+        // Recursively calculates Fibonacci numbers
+        public long Fibonacci(long n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return n;
+            }
+            else
+            {
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+            }
+        }
+
+        // Append text to outputTextBox in UI thread
+        public void AppendText(String text)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new MethodInvoker(() => AppendText(text)));
+            }
+            else
+            {
+                outputTextBox.AppendText(text + "\r\n");
+            }
+        }
+    }
+}
